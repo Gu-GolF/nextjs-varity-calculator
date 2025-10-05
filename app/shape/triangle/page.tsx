521b4f9387c79947ff0a05d2174f9bbdeaff2e02
Gu-GolF <s6552c10031@sau.ac.th>
2025-10-05 16:21:36 +0700

    create new project varity calculator

---
+++ app/shape/triangle/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <>
+     <h1>triangle สามเหลี่ยม</h1> 
+    </>
+  );
+}
---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <>
+      <div>
+        <h1 className="text-center text-lg text-gray-800">
+            Created by GuGolF DTI-SAU
+        </h1>
+        <h1 className="text-center text-lg text-gray-800">
+            copyright &copy;  2025 Southeast Asia University
+        </h1>
+      </div>
+    </>
+  );
+}
